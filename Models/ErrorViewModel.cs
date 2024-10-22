d990c628ba73eb5ca863ab6c9ab85dad0692d850
silenceafter <a59-info@yandex.ru>
2024-10-22 14:09:07 +0300

    22.10.2024

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace abstract_factory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

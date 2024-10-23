cebb93bd9871284c2bf6f299cd79b1de58de39de
silenceafter <a59-info@yandex.ru>
2024-10-23 16:23:29 +0300

    23.10.2024 #3

--- Products/Russian/RussianImage.cs
+++ Products/Russian/RussianImage.cs
@@ -11,12 +11,12 @@ namespace abstract_factory.Products.Russian
             {
                 Images = new Dictionary<string, string> 
                 { 
-                    { "Moscow-1-h", "/images/russian-1.jpg" }, 
-                    { "Moscow-2-v", "/images/russian-2.jpg" }, 
-                    { "SaintPetersburg-3-v", "/images/russian-3.jpg" }, 
-                    { "SaintPetersburg-4-v", "/images/russian-4.jpg" }, 
-                    { "Kazan-5-h", "/images/russian-5.jpg" }, 
-                    { "Moscow-6-h", "/images/russian-6.jpg" } 
+                    { "image-1-h", "/images/russian-1.jpg" }, 
+                    { "image-2-v", "/images/russian-2.jpg" }, 
+                    { "image-3-v", "/images/russian-3.jpg" }, 
+                    { "image-4-v", "/images/russian-4.jpg" }, 
+                    { "image-5-h", "/images/russian-5.jpg" }, 
+                    { "image-6-h", "/images/russian-6.jpg" } 
                 }               
             };
         }
